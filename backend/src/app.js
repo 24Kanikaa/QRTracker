3950d4b837d156634be01fb3d6805ad5b19e8bfd
24Kanikaa <kanikakainthla24@gmail.com>
2026-07-21 12:41:24 +0530

    removeconsole

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,10 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
-app.use((req, res, next) => {
-    console.log("Incoming:", req.method, req.originalUrl);
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log("Incoming:", req.method, req.originalUrl);
+//     next();
+// });
 app.use("/api", routes);
 
 app.use(errorHandler);
