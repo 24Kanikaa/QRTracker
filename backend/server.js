a1e7e515519ee9593941e8ab7861c7dc583aba98
24Kanikaa <kanikakainthla24@gmail.com>
2026-07-21 12:22:35 +0530

    change port

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,15 @@ require("dotenv").config();
 const app = require("./src/app");
 const initialize = require("./src/database/init");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 (async () => {
     try {
 
         await initialize();
 
-        app.listen(PORT, () => {
-            console.log("--------------------------------");
-            console.log(`🚀 Server running on port ${PORT}`);
-            console.log("--------------------------------");
+        app.listen(PORT, "0.0.0.0", () => {
+        console.log(`Server running on port ${PORT}`);
         });
 
     } catch (err) {
